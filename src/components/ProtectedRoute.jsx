bd8ef61311c929640bbdaf9c6974009e58c6537d
Sumit Jangir <sja@leapwork.com>
2025-03-16 12:25:11 +0530

    ffs

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from "react";
-import {
-  Navigate,
-  useNavigate,
-  useLocation,
-} from "react-router-dom";
+import { Navigate, useNavigate, useLocation } from "react-router-dom";
 import { mockApi } from "../lib/mockApi";
 
 const routesForRoles = {
-    Admin: ["/", "/add-collector", "/assign-retail"],
-    Collector: ["/"],
-    RetailUser: ["/"],
+  Admin: ["/", "/add-collector", "/assign-retail"],
+  Collector: ["/"],
+  RetailUser: ["/"],
 };
 
 export default function ProtectedRoute({ children }) {
@@ -30,8 +26,8 @@ export default function ProtectedRoute({ children }) {
   if (!user) return <Navigate to="/signin" replace />;
 
   if (routesForRoles[user.role].includes(location.pathname)) {
-    return children;    
-  }else{
+    return children;
+  } else {
     return <Navigate to="/" replace />;
   }
 }
